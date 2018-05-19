578701e695f5f3279fa6266c7585f6a1a7259fc0
oliverjam <oliverjamesphillips@gmail.com>
2018-05-19 22:01:49 +0100

    create jadenizer component

---
+++ workshop/jadenizer/jadenizer.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import jadenCase from '../utils/jadenCase';
+
+class Jadenizer extends React.Component {
+  state = { input: '', output: '' };
+
+  jadenize = event => {
+    event.preventDefault();
+    this.setState({ output: jadenCase(this.state.input) });
+  };
+
+  render() {
+    const { input, output } = this.state;
+    return (
+      <section className="section section--jaden" id="jadenizer">
+        <form onSubmit={this.jadenize}>
+          <h2>Jadenizer</h2>
+          <label htmlFor="converter-input">
+            Enter text for Jadenization
+            <input
+              id="jadenizer-input"
+              className="form__input"
+              value={input}
+              onChange={e => this.setState({ input: e.target.value })}
+            />
+          </label>
+          <button type="submit" className="form__button form__button--jaden">
+            Jadenize
+          </button>
+        </form>
+        {output && <div>{output}</div>}
+      </section>
+    );
+  }
+}
+
+export default Jadenizer;
