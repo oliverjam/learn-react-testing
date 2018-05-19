981e6cee360711eeef3049736a3c2dc41a7397c8
oliverjam <oliverjamesphillips@gmail.com>
2018-05-19 22:02:35 +0100

    dont try to capitalise empty values

--- workshop/utils/jadenCase.js
+++ workshop/utils/jadenCase.js
@@ -1,7 +1,7 @@
 function jadenCase(string) {
   const wordsArray = string.toLowerCase().split(' ');
   const capitalise = word => word[0].toUpperCase() + word.slice(1);
-  const capitalisedArray = wordsArray.map(capitalise);
+  const capitalisedArray = wordsArray.filter(Boolean).map(capitalise);
   return capitalisedArray.join(' ');
 }
 
