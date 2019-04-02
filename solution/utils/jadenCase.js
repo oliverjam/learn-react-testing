3adad439319ef71513c78391cf1dc72d5add7420
oliver.phillips <oliver.phillips@ticketmaster.co.uk>
2019-04-02 17:24:31 +0100

    Delete duplicate solution files

--- solution/utils/jadenCase.js
+++
@@ -1,8 +0,0 @@
-function jadenCase(string) {
-  const wordsArray = string.toLowerCase().split(' ');
-  const capitalise = word => word[0].toUpperCase() + word.slice(1);
-  const capitalisedArray = wordsArray.filter(Boolean).map(capitalise);
-  return capitalisedArray.join(' ');
-}
-
-export default jadenCase;
