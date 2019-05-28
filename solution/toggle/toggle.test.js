0a4242388f6aaa0ea706fa0194f14de29ae85e9d
oliver.phillips <oliver.phillips@ticketmaster.co.uk>
2019-05-28 12:54:48 +0100

    Remove unneeded vanilla test

--- solution/toggle/toggle.test.js
+++ solution/toggle/toggle.test.js
@@ -1,20 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import TestUtils from "react-dom/test-utils";
 import { render, fireEvent } from "react-testing-library";
-import Toggle from "../../workshop/toggle/toggle.js";
+import Toggle from "../../workshop/toggle/toggle";
 
-test("The toggle toggles when clicked", () => {
-  const root = document.createElement("div");
-  ReactDOM.render(<Toggle>just clicked</Toggle>, root);
-  document.body.appendChild(root);
-  const buttonNode = root.querySelector("button");
-  buttonNode.click();
-  const childrenNode = root.lastChild;
-  expect(childrenNode.textContent).toEqual("just clicked");
-});
-
-//react-testing-library solution
 test("The toggle toggles when clicked", () => {
   const { getByText } = render(<Toggle>just clicked</Toggle>);
   const buttonNode = getByText(/show/i);
