9e7481aa71d338e3f22935f63f37927acff8e1ec
oliver.phillips <oliver.phillips@ticketmaster.co.uk>
2019-05-28 13:01:34 +0100

    Update both components to use hooks

--- workshop/jadenizer/jadenizer.js
+++ workshop/jadenizer/jadenizer.js
@@ -1,40 +1,35 @@
 import React from "react";
 import jadenCase from "../utils/jadenCase";
 
-class Jadenizer extends React.Component {
-  state = {
-    input: "",
-    output: ""
-  };
+function Jadenizer() {
+  const [input, setInput] = React.useState("");
+  const [output, setOutput] = React.useState("");
 
-  jadenize = event => {
+  const handleSubmit = event => {
     event.preventDefault();
-    this.setState({ output: jadenCase(this.state.input) });
+    const jadenizedString = jadenCase(input);
+    setOutput(jadenizedString);
   };
-
-  render() {
-    const { input, output } = this.state;
-    return (
-      <section className="section section--jaden" id="jadenizer">
-        <form onSubmit={this.jadenize}>
-          <h2>Jadenizer</h2>
-          <label htmlFor="jadenizer-input">
-            Enter text for Jadenization
-            <input
-              id="jadenizer-input"
-              className="form__input"
-              value={input}
-              onChange={e => this.setState({ input: e.target.value })}
-            />
-          </label>
-          <button type="submit" className="form__button form__button--jaden">
-            Jadenize
-          </button>
-        </form>
-        {output && <output data-testid="output">{output}</output>}
-      </section>
-    );
-  }
+  return (
+    <section className="section section--jaden" id="jadenizer">
+      <form onSubmit={handleSubmit}>
+        <h2>Jadenizer</h2>
+        <label htmlFor="jadenizer-input">
+          Enter text for Jadenization
+          <input
+            id="jadenizer-input"
+            className="form__input"
+            value={input}
+            onChange={e => setInput(e.target.value)}
+          />
+        </label>
+        <button type="submit" className="form__button form__button--jaden">
+          Jadenize
+        </button>
+      </form>
+      {output && <output data-testid="output">{output}</output>}
+    </section>
+  );
 }
 
 export default Jadenizer;
--- workshop/markdownifier/markdownifier.js
+++ workshop/markdownifier/markdownifier.js
@@ -2,48 +2,45 @@ import React from "react";
 import "whatwg-fetch"; // node has no fetch so we need to polyfill for the tests
 import jadenCase from "../utils/jadenCase";
 
-class Markdownifier extends React.Component {
-  state = { input: "", output: "" };
+function Markdownifier() {
+  const [input, setInput] = React.useState("");
+  const [output, setOutput] = React.useState("");
 
-  markdownifier = event => {
+  const handleSubmit = event => {
     event.preventDefault();
     const res = fetch("https://micro-marked-nqbbqbtkrq.now.sh/", {
       method: "POST",
-      body: JSON.stringify({ markdown: this.state.input })
+      body: JSON.stringify({ markdown: input })
     })
       .then(res => res.text())
-      .then(html => this.setState({ output: html }));
+      .then(html => setOutput(html));
   };
-
-  render() {
-    const { input, output } = this.state;
-    return (
-      <section className="section section--markdown" id="markdownifier">
-        <form onSubmit={this.markdownifier}>
-          <h2>Markdownifier</h2>
-          <label htmlFor="markdown-input">
-            Enter markdown
-            <textarea
-              id="markdown-input"
-              className="form__input form__input--markdown"
-              value={input}
-              onChange={e => this.setState({ input: e.target.value })}
-              rows="6"
-            />
-          </label>
-          <button type="submit" className="form__button form__button--markdown">
-            Markdownify
-          </button>
-        </form>
-        {output && (
-          <output
-            data-testid="output"
-            dangerouslySetInnerHTML={{ __html: output }}
+  return (
+    <section className="section section--markdown" id="markdownifier">
+      <form onSubmit={handleSubmit}>
+        <h2>Markdownifier</h2>
+        <label htmlFor="markdown-input">
+          Enter markdown
+          <textarea
+            id="markdown-input"
+            className="form__input form__input--markdown"
+            value={input}
+            onChange={e => setInput(e.target.value)}
+            rows="6"
           />
-        )}
-      </section>
-    );
-  }
+        </label>
+        <button type="submit" className="form__button form__button--markdown">
+          Markdownify
+        </button>
+      </form>
+      {output && (
+        <output
+          data-testid="output"
+          dangerouslySetInnerHTML={{ __html: output }}
+        />
+      )}
+    </section>
+  );
 }
 
 export default Markdownifier;
